668b9213f1d284bbc7ade783b254535d3bc6bf6b
Satya <satyajeetkoule15@gmail.com>
2021-05-19 18:15:05 +0530

    Final

---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order online for Touchless Deleivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order online for Touchless Deleivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order online for Touchless Deleivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order online for Touchless Deleivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  z-index: 10;
+`;
+
+export default Home;
